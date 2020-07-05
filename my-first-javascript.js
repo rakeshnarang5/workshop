92773ffebb1d8ed8c5c2dc0ee3b73ce7a92e9335
Rakesh Narang <rakeshnarang5@gmail.com>
2020-07-05 22:33:05 +0530

    Create my-first-javascript.js

---
+++ my-first-javascript.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<br>
+
+<br>
+
+<button type="button"
+onClick="document.getElementById('more').innerHTML = '<p>Hi! My name is Rakesh!</p>'">Click me again</button>
+
+<p id="demo"></p>
+
+<p id="more"></p>
+
+
+
+</body>
+</html> 
