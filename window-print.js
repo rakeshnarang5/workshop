712dddbd6cbc54244df4410e70fcdc4aa29d3c7a
Rakesh Narang <rakeshnarang5@gmail.com>
2020-07-05 22:53:38 +0530

    Create window-print.js

---
+++ window-print.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2>The window.print() Method</h2>
+
+<p>Click the button to print the current page.</p>
+
+<h1>This is something that I have learnt for the first time in JS</h1>
+
+<button onclick="myFunc()">Print this page</button>
+
+<script type="text/javascript">
+	function myFunc() {
+    	window.print();
+    }
+</script>
+
+</body>
+</html>
