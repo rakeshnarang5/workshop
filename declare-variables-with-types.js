b3013e3e99e183e7fefa3e68bb6bf8f4c048f278
Rakesh Narang <rakeshnarang5@gmail.com>
2020-07-05 23:22:27 +0530

    Create declare-variables-with-types.js

---
+++ declare-variables-with-types.js
@@ -0,0 +1,3 @@
+var x = new String();        // Declares x as a String object
+var y = new Number();        // Declares y as a Number object
+var z = new Boolean();       // Declares z as a Boolean object
